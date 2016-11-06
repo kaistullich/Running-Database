62e064c72e1507e3f58cc17989675524fb0eda99
Kai Stullich <kaistullich@rocketmail.com>
2016-11-06 12:24:21 -0800

    changed placeholders to  current Python3 string formatting

--- running.py
+++ running.py
@@ -9,17 +9,17 @@ if __name__ == "__main__":
     def create_table():
         c.execute('CREATE TABLE IF NOT EXISTS running_data(date TEXT, distance REAL, duration REAL, avg_pace REAL)')
     
-    date = input('Running date: ')
-    distance = float(input('Enter TOTAL DISTANCE RAN: '))
-    duration = float(input('Enter TOTAL DURATION: '))
-    avg_pace = float(input('Enter AVERAGE PACE: '))
+    date = input('Running the date: ')
+    distance = float(input('Enter TOTAL DISTANCE RAN (i.e 2.11): '))
+    duration = float(input('Enter TOTAL DURATION (i.e. 20.34): '))
+    avg_pace = float(input('Enter AVERAGE PACE (i.e. 10.44): '))
     
     def data_entry():
-        c.execute("INSERT INTO running_data VALUES (%s, %.2f, %.2f, %.2f)" % (date, distance, duration, avg_pace))
+        c.execute("INSERT INTO running_data VALUES ({}, {}, {}, {})".format(date, distance, duration, avg_pace))
         conn.commit()
         c.close()
         conn.close()
 
-
-    create_table()
+    #Only run 'create_table()' for the FIRST time
+    #create_table()
     data_entry()
