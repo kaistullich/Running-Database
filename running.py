0bb8ca81cb6bfd35253a2b44ec851996e490d498
Kai Stullich <kaistullich@rocketmail.com>
2016-11-07 10:29:13 -0800

    Added ID column, and removed counter in for loop

--- running.py
+++ running.py
@@ -1,7 +1,7 @@
 import sqlite3
 import time
 
-conn = sqlite3.connect('test_db.db')
+conn = sqlite3.connect('test_database.db')
 c = conn.cursor()
 
 def start_menu():
@@ -21,7 +21,7 @@ def update_menu():
 if __name__ == "__main__":
 
     def create_table():
-        c.execute('CREATE TABLE IF NOT EXISTS data_test(date TEXT, distance REAL, duration REAL, avg_pace REAL)')
+        c.execute('CREATE TABLE IF NOT EXISTS database_test(id INTEGER NOT NULL PRIMARY KEY, date TEXT, distance REAL, duration REAL, avg_pace REAL)')
     create_table()
     
     choice = ""
@@ -33,17 +33,19 @@ if __name__ == "__main__":
         # INSERTING into Database
         if choice == '1':
             try:
-                date = input('\n\n\nA) Enter the DATE for the run completed (i.e. 2016-01-10): ')
+                id = int(input('\n\n\nA) Enter ID #: '))
+                print ('\n      ++ ID Entered into Database ++')
+                date = input('\nB) Enter the DATE for the run completed (i.e. 2016-01-10): ')
                 print ('\n      ++ Date Entered into Database ++')
-                distance = float(input('\nB) Enter TOTAL DISTANCE RAN (i.e 2.11): '))
+                distance = float(input('\nC) Enter TOTAL DISTANCE RAN (i.e 2.11): '))
                 print ('\n      ++ Distance Entered into Database ++')
-                duration = float(input('\nC) Enter TOTAL DURATION (i.e. 20.34): '))
+                duration = float(input('\nD) Enter TOTAL DURATION (i.e. 20.34): '))
                 print ('\n      ++ Duration Entered into Database ++')
-                avg_pace = float(input('\nD)Enter AVERAGE PACE (i.e. 10.44): '))
+                avg_pace = float(input('\nE)Enter AVERAGE PACE (i.e. 10.44): '))
                 print ('\n      ++ Average Pace Entered into Database ++')
                 
                 def data_entry():
-                    c.execute("INSERT INTO data_test VALUES (?, ?, ?, ?)", (date, distance, duration, avg_pace))
+                    c.execute("INSERT INTO database_test VALUES (?, ?, ?, ?, ?)", (id, date, distance, duration, avg_pace))
                     conn.commit()
 
                 data_entry()
@@ -67,23 +69,21 @@ if __name__ == "__main__":
         # PRINT CURRENT Database
         if choice == '2':
             def read_from_db():
-                c.execute('SELECT * FROM data_test')
+                c.execute('SELECT * FROM database_test')
                 print ('\n        <CURRENT DATABASE DATA>\n')
-                a = 0
                 for row in c.fetchall():
-                    a+= 1
-                    print (a,row,'\n')
+                    print (row,'\n')
                 return read_from_db
             read_from_db()
         
         # UPDATING the Database
         if choice == '3':
             def update_db():
-                c.execute('SELECT * FROM data_test')
+                c.execute('SELECT * FROM database_test')
                 # 'i' prints current database information
                 i = read_from_db()
                 print (i)
-                update_column.lower() = input('Enter column name you want to update: ')
+                update_column = input('Enter column name you want to update: ')
                 # prints the menu for either updating DATE or all other columns
                 update_menu()
                 update_choice = input('\nEnter choice number from menu: ')
@@ -93,16 +93,16 @@ if __name__ == "__main__":
                 if update_choice == '2':
                     all_other_rows = float(input('Enter the NEW value (i.e. 3.11): '))
                     return all_other_rows
-                c.execute('UPDATE running_data SET (?) = (?) WHERE (?) = (?)', (update_column))
+                c.execute('UPDATE database_test SET (?) = (?) WHERE (?) = (?)', (update_column.lower()))
                 conn.commit()
             update_db()
         
         # DELETING rows in the Database
         if choice == '4':
             def delete_db():
-                c.execute('SELECT * FROM running_data')
+                c.execute('SELECT * FROM database_test')
                 # EDIT THE (?)
-                c.execute('DELETE FROM running_data WHERE (?) = (?)', ())
+                c.execute('DELETE FROM database_test WHERE (?) = (?)', ())
                 conn.commit()
             delete_db()
         
