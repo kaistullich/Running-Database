18ecbc4421258e3a06b5eec63c498b7e5daee214
Kai Stullich <kaistullich@rocketmail.com>
2016-11-06 18:02:24 -0800

    Added try/except block and added user menu

--- running.py
+++ running.py
@@ -3,23 +3,47 @@ import sqlite3
 conn = sqlite3.connect('running_stats.db')
 c = conn.cursor()
 
+def menu():
+    print ('\n------------------------')
+    print ('    RUNNING DATABASE')
+    print ('------------------------\n')
+    print ('1) Enter Data')
+    print ('0) EXIT')
 
 if __name__ == "__main__":
 
-    def create_table():
-        c.execute('CREATE TABLE IF NOT EXISTS running_data(date TEXT, distance REAL, duration REAL, avg_pace REAL)')
-    
-    date = input('Enter DATE (i.e. 2016-01-10): ')
-    distance = float(input('Enter TOTAL DISTANCE RAN (i.e 2.11): '))
-    duration = float(input('Enter TOTAL DURATION (i.e. 20.34): '))
-    avg_pace = float(input('Enter AVERAGE PACE (i.e. 10.44): '))
-    
-    def data_entry():
-        c.execute("INSERT INTO running_data VALUES (?, ?, ?, ?)", (date, distance, duration, avg_pace))
-        conn.commit()
-        c.close()
-        conn.close()
+    choice = ""
+    while (choice != '0'):
+        menu()
+        choice = input('\nEnter number from menu above to continue: ')
+        print ('-------------------------------------------')
+        if choice == '1':
+            try:
+                def create_table():
+                    c.execute('CREATE TABLE IF NOT EXISTS running_data(date TEXT, distance REAL, duration REAL, avg_pace REAL)')
+                
+                date = input('\n\n\nA) Enter the DATE for the run completed (i.e. 2016-01-10): ')
+                print ('\n      ++ Date Entered into Database ++')
+                distance = float(input('\nB) Enter TOTAL DISTANCE RAN (i.e 2.11): '))
+                print ('\n      ++ Distance Entered into Database ++')
+                duration = float(input('\nC) Enter TOTAL DURATION (i.e. 20.34): '))
+                print ('\n      ++ Duration Entered into Database ++')
+                avg_pace = float(input('\nD)Enter AVERAGE PACE (i.e. 10.44): '))
+                print ('\n      ++ Average Pace Entered into Database ++')
+                
+                def data_entry():
+                    c.execute("INSERT INTO running_data VALUES (?, ?, ?, ?)", (date, distance, duration, avg_pace))
+                    conn.commit()
+                    c.close()
+                    conn.close()
 
-    
-    create_table()
-    data_entry()
+                create_table()
+                data_entry()
+            
+            except Exception as e:
+                print ('\n\n*************************')
+                print ('ERROR FOUND! Re-Do Entry!:',e)
+                print ('*************************')
+        
+        if choice == '0':
+            print ('\n%% EXITED %%')
