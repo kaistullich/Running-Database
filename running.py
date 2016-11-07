18438884eca645a9b4e095927a3ec90123abc8a3
Kai Stullich <kaistullich@rocketmail.com>
2016-11-06 16:39:23 -0800

    Fixed string formatting to midigate SQL injeection vulnerablilties

--- running.py
+++ running.py
@@ -1,4 +1,5 @@
 import sqlite3
+from datetime import datetime,date
 
 conn = sqlite3.connect('running_stats.db')
 c = conn.cursor()
@@ -9,17 +10,17 @@ if __name__ == "__main__":
     def create_table():
         c.execute('CREATE TABLE IF NOT EXISTS running_data(date TEXT, distance REAL, duration REAL, avg_pace REAL)')
     
-    date = input('Running the date: ')
+    date = input('Enter DATE (i.e. 2016-01-10): ')
     distance = float(input('Enter TOTAL DISTANCE RAN (i.e 2.11): '))
     duration = float(input('Enter TOTAL DURATION (i.e. 20.34): '))
     avg_pace = float(input('Enter AVERAGE PACE (i.e. 10.44): '))
     
     def data_entry():
-        c.execute("INSERT INTO running_data VALUES ({}, {}, {}, {})".format(date, distance, duration, avg_pace))
+        c.execute("INSERT INTO running_data VALUES (?, ?, ?, ?)", (date, distance, duration, avg_pace))
         conn.commit()
         c.close()
         conn.close()
 
-    #Only run 'create_table()' for the FIRST time
-    #create_table()
+    
+    create_table()
     data_entry()
